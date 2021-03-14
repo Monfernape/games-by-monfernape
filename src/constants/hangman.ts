b8bca399ae60cf2f48dd6db3f52746b21adc9b90
Monfernape <engr.usmankhalil@gmail.com>
2021-03-14 23:16:17 +0500

    /* eslint-disable react-hooks/exhaustive-deps */

--- src/constants/hangman.ts
+++ src/constants/hangman.ts
@@ -1,33 +1,35 @@
-import { IAlphabet } from "../models"
+import { IAlphabet } from "../models";
 
-export const RANDOM_WORD_API_BASE_URL: string = "https://random-words-api.vercel.app/word"
+export const RANDOM_WORD_API_BASE_URL: string =
+  "https://random-words-api.vercel.app/word";
+
+export const DEFAULT_NUMBER_OF_TRIES = 5;
 
 export const Alphabet: IAlphabet[] = [
-  { letter: "A", pressed: false},
-  { letter: "B", pressed: false},
-  { letter: "C", pressed: false},
-  { letter: "D", pressed: false},
-  { letter: "E", pressed: false},
-  { letter: "F", pressed: false},
-  { letter: "G", pressed: false},
-  { letter: "H", pressed: false},
-  { letter: "I", pressed: false},
-  { letter: "J", pressed: false},
-  { letter: "K", pressed: false},
-  { letter: "L", pressed: false},
-  { letter: "M", pressed: false},
-  { letter: "N", pressed: false},
-  { letter: "O", pressed: false},
-  { letter: "P", pressed: false},
-  { letter: "Q", pressed: false},
-  { letter: "R", pressed: false},
-  { letter: "S", pressed: false},
-  { letter: "T", pressed: false},
-  { letter: "U", pressed: false},
-  { letter: "V", pressed: false},
-  { letter: "W", pressed: false},
-  { letter: "X", pressed: false},
-  { letter: "Y", pressed: false},
-  { letter: "Z", pressed: false},
+  { letter: "A", pressed: false },
+  { letter: "B", pressed: false },
+  { letter: "C", pressed: false },
+  { letter: "D", pressed: false },
+  { letter: "E", pressed: false },
+  { letter: "F", pressed: false },
+  { letter: "G", pressed: false },
+  { letter: "H", pressed: false },
+  { letter: "I", pressed: false },
+  { letter: "J", pressed: false },
+  { letter: "K", pressed: false },
+  { letter: "L", pressed: false },
+  { letter: "M", pressed: false },
+  { letter: "N", pressed: false },
+  { letter: "O", pressed: false },
+  { letter: "P", pressed: false },
+  { letter: "Q", pressed: false },
+  { letter: "R", pressed: false },
+  { letter: "S", pressed: false },
+  { letter: "T", pressed: false },
+  { letter: "U", pressed: false },
+  { letter: "V", pressed: false },
+  { letter: "W", pressed: false },
+  { letter: "X", pressed: false },
+  { letter: "Y", pressed: false },
+  { letter: "Z", pressed: false },
 ];
-
